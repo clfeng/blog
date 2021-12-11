166132d5e0ace970b5d9a15ceefc07148e97bcb1
clfeng <9331500-clfeng1@users.noreply.gitlab.com>
2021-12-11 16:35:44 +0800

    feat: 添加示例

---
+++ other/gtmc/vue_demo/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <span @click="onShowBtn">showBtn</span>
+    <button @click="onClick" v-if="showBtn">button</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      showBtn: false
+    }
+  },
+  methods: {
+    onClick () {
+      console.log('click event');
+    },
+    onShowBtn () {
+      this.showBtn = true;
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
